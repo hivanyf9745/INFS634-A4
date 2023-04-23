7c31ca3704245649d6440b1e0766619f8695f696
Ivan Huang <yifanhuang.Ivan@hotmail.com>
2023-04-23 15:48:14 -0400

    Created the initial files for INFS634 final project

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php include "includes/header.php"; ?>
+  <title>PACE | Your Workflow Management System</title>
+</head>
+<body>
+  <div class="align-items-center d-none d-lg-flex">
+    <div class="col-4 logo-container">
+      <img src="/INFS634-A4/imgs/main-logo.svg" alt="main-logo" class="main-logo-img"/>
+    </div>
+    <div class="col-8 login-container">
+      <div class="container login-inner">
+      </div>
+    </div>
+  </div>
+<?php include "includes/footer.php"; ?>
